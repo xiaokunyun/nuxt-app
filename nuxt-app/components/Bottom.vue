9372decf49f64c2d24eecf64ee9e18b33328d884
xuxiaokun <xuxiaokun@cnrmall.com>
2022-09-01 18:10:45 +0800

    3

---
+++ nuxt-app/components/Bottom.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    底部
+  </div>
+</template>
---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative w-screen h-screen">
+    <Head class=""/>
+    <slot />
+    <Bottom/>
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
